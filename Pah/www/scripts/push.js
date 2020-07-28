3129068e477a947061e43ea9931df6c0d42303be
Oscar Diaz <gamerchiken@hotmail.com>
2020-07-28 11:51:46 -0500

    Style edit
    - Save 1, "FONDO/SAVINGS"

--- Pah/www/scripts/push.js
+++ Pah/www/scripts/push.js
@@ -1,4 +1,9 @@
-function pushTest(){
-    const navigator = document.querySelector('#navigator');
-    navigator.pushPage('detailGoal.html');
+function pushTest() {
+  const navigator = document.querySelector("#navigator");
+  navigator.pushPage("detailGoal.html");
+}
+
+function pushTest2() {
+    const navigator = document.querySelector("#navigator");
+    navigator.pushPage("detailSaving.html");    
 }
