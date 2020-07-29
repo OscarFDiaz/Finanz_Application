ce8d7bb63dcec50ffad7aa865bff2c0f2ca13eb0
Oscar Diaz <gamerchiken@hotmail.com>
2020-07-29 11:51:20 -0500

    Style edit
    Pantallas "META/GOALS" terminada

--- Pah/platforms/browser/www/scripts/push.js
+++ Pah/platforms/browser/www/scripts/push.js
@@ -7,3 +7,8 @@ function pushTest2() {
     const navigator = document.querySelector("#navigator");
     navigator.pushPage("detailSaving.html");    
 }
+
+function pushTest3() {
+  const navigator = document.querySelector("#navigator");
+  navigator.pushPage("newGoal.html");    
+}
